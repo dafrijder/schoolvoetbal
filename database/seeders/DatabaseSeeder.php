4f28920f16129bc3c4dbc604451852db93a673fe
Jorran006 <Jorranderooij@gmail.com>
2025-11-14 14:01:39 +0100

    userToAdminSeeder probleem opgelost

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,12 +17,12 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
 
-        $this->call(\Database\Seeders\AdminUserSeeder::class);
+
+        $this->call([
+            AdminUserSeeder::class,
+            MakeUserAdminSeeder::class,
+        ]);
     }
 }
 
