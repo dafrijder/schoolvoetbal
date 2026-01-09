442504b17297e97db3ad244e85bfda71c503c410
Dafrijder <nielsdegast@outlook.com>
2026-01-09 13:46:21 +0100

    admin link added

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -3,5 +3,8 @@
         <li><a href="{{ route('home') }}">Home</a></li>
         <li><a href="{{ route('games.index') }}">Games</a></li>
         <li><a href="{{ route('teams.index') }}">Teams</a></li>
+        @if(auth()->user() && auth()->user()->is_admin)
+            <li><a href="{{ route('admin.index') }}">Beheer</a></li>
+        @endif
     </ul>
 </nav>
